7b7e53b030f8b61dd4ddf2e99477801015e8462b
TaeGyu Lee <tglee1127@naver.com>
2026-04-14 16:12:19 +0900

    260414_ch03

---
+++ ch03/priority.py
@@ -0,0 +1,18 @@
+print(2 * (3 - 5))
+
+num2 = 1 > (2 and 3) + 3
+print(num2)
+
+print(1 and 2)
+print(2 and 1)
+
+print("--------------")
+print(9<4 and 3>2)
+print(9>4 and 3>2)
+print(9<4 or 3>2)
+print(9<4 or 3<2)
+
+print("--------------")
+print(9<4 and 3>2 and 4>2)
+
+print((3-5)+3<1 and 3-5>1)
