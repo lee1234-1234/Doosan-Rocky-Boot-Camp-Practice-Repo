df47aba696742cd36dbdee49163c9068b5f83215
TaeGyu Lee <tglee1127@naver.com>
2026-04-14 16:11:52 +0900

    260414_ch03

---
+++ ch03/logic.py
@@ -0,0 +1,14 @@
+print(True  and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("--------------")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("--------------")
+print(not True)
+print(not False)
