ae2faa3027f63cd6084a7183dc139e542cb72a8c
TaeGyu Lee <tglee1127@naver.com>
2026-04-14 16:11:14 +0900

    260414_ch03

---
+++ ch03/if1.py
@@ -0,0 +1,28 @@
+score = 90
+if score >= 90:
+    print("합격입니다.")
+print("프로그램 종료")
+
+print("--------------")
+
+score = 60
+score = 88
+if score > 80:
+    print("합격입니다.")
+else:
+    print("불합격입니다.")
+print("프로그램 종료")
+
+print("--------------")
+na = 21
+if na % 2==0:
+    print(na, "짝수")
+print("if 문 종료 됨")
+
+print("--------------")
+na = 21
+if na % 2==0:
+    print(na, "짝수")
+else: 
+    print(na, "홀수")
+print("if 문 종료 됨")
