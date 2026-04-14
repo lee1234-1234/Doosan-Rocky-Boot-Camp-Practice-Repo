581aea24f66bd7b851d3bcb423b943af22a30730
TaeGyu Lee <tglee1127@naver.com>
2026-04-14 16:42:52 +0900

    260414_ch03

---
+++ ch03/week.py
@@ -0,0 +1,18 @@
+print("월요일부터 일요일 중 영어로 번역하고 싶은 요일을 입력하세요:")
+yoil = input()
+if yoil in ("월요일", "월"):
+    print("Monday")
+elif yoil == "화요일":
+    print("Tuesday")
+elif yoil == "수요일":
+    print("Wednesday")
+elif yoil == "목요일":
+    print("Thursday")
+elif yoil == "금요일":
+    print("Friday")
+elif yoil == "토요일":
+    print("Saturday")
+elif yoil == "일요일":
+    print("Sunday")
+else:
+    print("한글 요일을 잘못 입력했습니다.")
