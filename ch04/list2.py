d1ddc660bd1cbc43089e2650fcb90e0fc0e737ad
TaeGyu Lee <tglee1127@naver.com>
2026-04-15 13:53:49 +0900

    260415_ch04

---
+++ ch04/list2.py
@@ -0,0 +1,41 @@
+#값 추가하기
+lista = [1, 2, 3]
+print(lista)
+lista.append("append")
+print(lista)
+
+
+
+print("-------------------------")
+listc = []
+print(listc)
+print(type(listc))
+
+listc.append(300)
+print(listc)
+listc.append("파이썬")
+print(listc)
+listc.append(3.7)
+print(listc)
+listc.append(False)
+print(listc)
+listc.append([1, True])
+print(listc)
+
+print("-------------------------")
+subject = ['국어', '수학', '영어', '국사']
+print(subject)
+
+subject.append('영어')
+print(subject)
+
+subject.remove('영어')
+print(subject)
+
+print("-------------------------")
+clovers = ['클로버1', '클로버2', '클로버3']
+print(clovers[1])
+print(clovers)
+
+del clovers[1]
+print(clovers)
