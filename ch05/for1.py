a6104f46a4422f02d7b5150bc580c0927e6188b9
TaeGyu Lee <tglee1127@naver.com>
2026-04-16 14:35:05 +0900

    260416_ch05

---
+++ ch05/for1.py
@@ -0,0 +1,32 @@
+for num in [0, 1, 2]:
+    print(num)
+
+charcters = ['엘리스', '도도새', '3월토끼']
+for charcters in charcters:
+    print(charcters)
+
+print("-----------------")
+for letter in '체셔고양이':
+    print(letter)
+    
+print("-----------------")
+for letter in '체셔고양이':
+    print(letter, end="")
+    
+print("-----------------")
+for nums in [0,1,2]:
+    print(num)
+    print(nums)
+    
+print("-----------------")
+nums = [0, 1, 2]
+for num in nums:
+    print(num)
+    print(nums)
+    
+print("튜플-----------------")
+fruits = ('사과', '배', '바나나', '망고', '수박')
+
+print('내가 좋아하는 과일: ')
+for fruit in fruits:
+    print(fruit)
