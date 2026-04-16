799fb2d595c6168582274a4dbe66837876c24b6d
TaeGyu Lee <tglee1127@naver.com>
2026-04-16 15:33:22 +0900

    260416

---
+++ ch05/while2.py
@@ -0,0 +1,20 @@
+stra = "파이썬"
+strb = " 프로그래밍"
+stra = stra + strb
+print(stra)
+
+a = 10
+b = 5
+a = a - b
+print(a)
+
+a = 10
+b = 5
+a -= b
+print(a)
+
+print("-----------------")
+num = 0
+while num < 3:
+    print('안녕거북이', num)
+    num += 1
