4da876fd0a02989e3e54100d716ce3c2bc6c01c8
TaeGyu Lee <tglee1127@naver.com>
2026-04-16 14:47:56 +0900

    260415

---
+++ homework/4차시/hw2.py
@@ -0,0 +1,5 @@
+# 2. 앞서 생성한 movie_rank 리스트에 "위키드"를 추가하라.
+
+movie_rank = ['하얼빈', '무파사:라이온킹', '소방관']
+movie_rank.append('위키드')
+print(movie_rank)
---
+++ homework/4차시/hw4.py
@@ -0,0 +1,5 @@
+# 4. movie_rank 리스트에서 '소방관'을 삭제하라.
+
+movie_rank = [' 하얼빈', '무파사:라이온킹', '소방관', '모아나2', '위키드']
+movie_rank.remove('소방관')
+print(movie_rank)
