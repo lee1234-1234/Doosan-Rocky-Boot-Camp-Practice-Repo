e07d2e2ba696caa6918fc032794df322635a44ed
TaeGyu Lee <tglee1127@naver.com>
2026-04-17 11:14:34 +0900

    260416

---
+++ homework/5차시/4.py
@@ -0,0 +1,10 @@
+# 4. 리스트에는 동물이름이 문자열로 저장돼 있다.
+# 리스트 = ['dog', 'cat', 'parrot']
+# 동물 이름과 글자수를 다음과 같이 출력하라.
+# dog 3
+# cat 3
+# parrot 6
+
+리스트 = ['dog', 'cat', 'parrot']
+for i in 리스트:
+    print(i, len(i))
