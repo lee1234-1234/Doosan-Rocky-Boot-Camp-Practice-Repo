57a2a65b34e34e5427be4ce0962cf3b70565c434
TaeGyu Lee <tglee1127@naver.com>
2026-04-17 15:42:11 +0900

    260417_ch06

---
+++ ch06/func2.py
@@ -0,0 +1,27 @@
+# def funca(pa, pb):
+#     nc = pa + pb
+    
+# na = 10
+# nb = 11
+    
+# funca(na, nb)
+# print(na, "+", nb, "=", nc)
+
+na = 10
+nb = 11
+
+pa = na
+pb = nb
+
+nc = pa+pb
+print(nc)
+
+print('----------------')
+# def funca(pa, pb):
+#     nc = pa + pb
+    
+# na = 10
+# nb = 11
+    
+# funca(na, nb)
+# print(na, "+", nb, "=", nc)
