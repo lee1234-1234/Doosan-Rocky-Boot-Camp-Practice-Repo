1c7cadd31225a2f9180f3dfba9cedc605898eac5
TaeGyu Lee <tglee1127@naver.com>
2026-04-17 15:42:19 +0900

    260417_ch06

---
+++ ch06/func_reutrn.py
@@ -0,0 +1,10 @@
+def fplusminus(arg):
+    if arg > 0:
+        return "plus"
+    elif arg < 0:
+        return "minus"
+    else:
+        return "noooooooooo"
+    
+stra = fplusminus(0)
+print(stra)
