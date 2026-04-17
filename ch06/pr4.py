e465b0db2e8743ae0be35def7c2ea8643688fb31
TaeGyu Lee <tglee1127@naver.com>
2026-04-17 16:32:23 +0900

    260417_ch06

---
+++ ch06/pr4.py
@@ -0,0 +1,16 @@
+def fadd(num1, num2):
+    return num1 + num2
+
+def fsub(num1, num2):
+    return num1 - num2
+
+def fmul(num1, num2):
+    return num1 * num2
+
+def fdiv(num1, num2):
+    return num1 / num2
+
+print(fadd(100, 3))
+print(fsub(100, 3))
+print(fmul(100, 3))
+print(fdiv(100, 3))
