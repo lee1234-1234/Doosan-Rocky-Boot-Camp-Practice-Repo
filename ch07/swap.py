0e077f23c529ed8c9fef3ea0b766230ba92e38b8
TaeGyu Lee <tglee1127@naver.com>
2026-04-20 16:19:30 +0900

    260420_ch07

---
+++ ch07/swap.py
@@ -0,0 +1,11 @@
+def funca(pa, pb):
+    temp = pa
+    pa = pb
+    pb = temp
+    return pa, pb
+    
+na = 10
+nb = 11
+print("na 값은 ", na, "nb 값은 ", nb)
+na, nb = funca(na, nb)
+print("na 값은 ", na, "nb 값은 ", nb)
