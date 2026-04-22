65a4371357b22570e7e8ac50582c676c510cea17
TaeGyu Lee <tglee1127@naver.com>
2026-04-22 16:43:41 +0900

    260422_ch09

---
+++ ch09/built_in_class.py
@@ -0,0 +1,18 @@
+na = "10"
+na = int("10")
+print(type(na))
+
+print("------------------")
+strb = str("abc")
+print(strb)
+
+strc = strb.capitalize()
+print(strc)
+
+# class str:
+#     def __init__(self, data):
+#         self.data = data        #기능: 문자열로 변환
+        
+#     def capitalize(self):
+#         pass                    #기능: 첫자를 대문자로 변환
+#         return 첫자_대문자_문자열
