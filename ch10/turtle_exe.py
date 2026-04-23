d6dc18f51afee6f9a3bf551bbaabe0e6bb635663
TaeGyu Lee <tglee1127@naver.com>
2026-04-23 16:39:49 +0900

    260423_ch10

---
+++ ch10/turtle_exe.py
@@ -0,0 +1,12 @@
+# from turtle import *
+import turtle
+
+turtle.shape('turtle')
+
+# turtle.forward(100)
+
+# turtle.left(120)
+turtle.speed(0)
+for i in range(1, 1000, 1):
+    turtle.forward(i)
+    turtle.left(90)
