5a75e370a53182e0d345d760dbe263a45dbe6df0
TaeGyu Lee <tglee1127@naver.com>
2026-04-23 16:38:24 +0900

    260423_ch10

---
+++ ch10/Cvalue.py
@@ -0,0 +1,17 @@
+class Cvalue():
+    
+    def __init__(self):
+        self.lista = []
+        
+    def add(self, num):
+        self.lista.append(num)
+        
+    def fprint(self):
+        print(self.lista)
+    
+    
+p1 = Cvalue()
+p1.add(1)
+p1.add(2)
+p1.add(3)
+p1.fprint()
