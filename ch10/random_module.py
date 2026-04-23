cf2de2b4eb48be075e14d2a41534496fd09843ff
TaeGyu Lee <tglee1127@naver.com>
2026-04-23 16:39:44 +0900

    260423_ch10

---
+++ ch10/random_module.py
@@ -0,0 +1,9 @@
+import random
+
+animals = ["체셔고양이", "오리", "도도새", "호랑이", "코뿔소"]
+
+print(random.choice(animals))
+print(random.choice(animals))
+print(random.choice(animals))
+print(random.sample(animals, 4))
+print(random.randint(1, 45))
