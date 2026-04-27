83f75e92a7a642c1b9d37b044c2cfadd9c84a1d3
TaeGyu Lee <tglee1127@naver.com>
2026-04-27 21:42:04 +0900

    260427_ch12

---
+++ ch12/file_append.py
@@ -0,0 +1,12 @@
+path = r"ch12\file.txt"
+mode = "w"
+
+
+
+
+# "r" : read
+# "w" : write
+# "a" : append
+# "r+" : read + write => 파일을 읽고 쓰기(기 데이터 존재)
+# "w+" : write + read => 파일을 새로 만들거나 비우고 쓰고 읽기
+# "a+" : append + read
