34119460a052a199aaf8516703563604d59669a6
TaeGyu Lee <tglee1127@naver.com>
2026-04-29 20:55:31 +0900

    Advanced class session5

---
+++ Advanced_class_practice/Session5/1.py
@@ -0,0 +1,10 @@
+def add_cart(item):
+    print(item + "를 장바구니에 담았습니다.")
+
+add_cart("사과")
+add_cart("바나나")
+add_cart("딸기")
+
+# print("사과를 장바구니에 담았습니다.")
+# print("바나나를 장바구니에 담았습니다.")
+# print("딸기를 장바구니에 담았습니다.")
