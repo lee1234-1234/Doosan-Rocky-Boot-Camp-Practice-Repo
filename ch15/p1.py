84565c5cd61d5eae4fc126cf13afa8bea24c5d0e
TaeGyu Lee <tglee1127@naver.com>
2026-04-30 16:36:28 +0900

    260430_ch15

---
+++ ch15/p1.py
@@ -0,0 +1,13 @@
+foods = ["김밥", "만두", "양념치킨", "족발", "피자", "쫄면", "라면"]
+
+ifoods = iter(foods)
+# print(type(ifoods))
+
+for food in ifoods:
+    print(food)
+    
+# print(next(ifoods))
+# print(next(ifoods))
+# print(next(ifoods))
+# print(next(ifoods))
+# print(next(ifoods))
