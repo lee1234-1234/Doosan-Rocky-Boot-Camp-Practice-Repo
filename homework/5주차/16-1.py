cdc6d297f55365a1b119657c1fc72af312c05e41
TaeGyu Lee <tglee1127@naver.com>
2026-05-10 18:33:33 +0900

    260510

---
+++ homework/5주차/16-1.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.stack = ['두산', '로키', '부트']
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def peak(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return
+
+    def status_stack(self):
+        return self.stack
+    
+s1 = Stack()
+s1.push("캠프")
+print(s1.status_stack())
