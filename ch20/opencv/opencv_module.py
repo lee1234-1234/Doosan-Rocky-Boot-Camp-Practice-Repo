7188a379a361012039470b71470ecabe7a9615cd
TaeGyu Lee <tglee1127@naver.com>
2026-05-11 14:53:54 +0900

    260511_ch20

---
+++ ch20/opencv/opencv_module.py
@@ -0,0 +1,11 @@
+# opencv_module.py
+
+import cv2
+
+# 이미지 파일 읽기
+image = cv2.imread(r'ch20\opencv\sample.jpg')
+
+cv2.imshow('Loaded Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.destroyAllWindows()
