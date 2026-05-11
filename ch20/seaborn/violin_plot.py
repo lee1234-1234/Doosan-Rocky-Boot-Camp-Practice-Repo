f2994cbb2e5eb09e960c9ccfaaca58fdf48a2809
TaeGyu Lee <tglee1127@naver.com>
2026-05-11 13:46:29 +0900

    260511

---
+++ ch20/seaborn/violin_plot.py
@@ -0,0 +1,20 @@
+# volion_plot.py
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris = sns.load_dataset("iris")
+
+sns.violinplot(data=iris, 
+            x='species',
+            y='sepal_length',
+            inner='quart')
+
+plt.title('Violin Plot Example')
+plt.show()
+
+# inner="quart"    # 사분위수 표현
+# None => 내부 표시 안함
+# point => 각 데이터 그룹의 평균값을 점으로 표시
+# box => 박스 플롯 모양
+# stick => 각 데이터 포인트를 세로선 표시
